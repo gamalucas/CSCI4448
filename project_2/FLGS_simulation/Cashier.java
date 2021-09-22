61b17d74b866e409645ded0b88524199c765c30a
gamalucas <luga2469@colorado.edu>
2021-09-22 15:02:02 -0600

    added comments on cashier file

--- project_2/FLGS_simulation/Cashier.java
+++ project_2/FLGS_simulation/Cashier.java
@@ -5,10 +5,6 @@ public class Cashier extends Employee{
     public Cashier(String name){
         this.employee_name = name;
     }
-
-    // public void test(int dayss){
-    //     System.out.println(employee_name + " the cahsier has arrived at day " + dayss);
-    // }
     
     public int getRandomNumber(int min, int max) { //https://www.baeldung.com/java-generating-random-numbers-in-range
         return (int) ((Math.random() * (max - min)) + min);
@@ -35,38 +31,44 @@ public class Cashier extends Employee{
 
     public void Vacuum(List<Games> shelf, Map<String, Integer> DamageContainer){
         System.out.println(employee_name + " is vacuuming the store ");
-        int demagePossibility = getRandomNumber(0, 101);
+        int demagePossibility = getRandomNumber(0, 101); // generate a random number that will reflect in the probability of a employee to break a game while vacumming the store
+        int brokenGame = 0;
         if(employee_name == "Burt" & demagePossibility >= 0 & demagePossibility <= 10){
             //generate a random number from 1 to 12 (because there are 12 fixed game titles). The selected number will represent which game to remove
-            demagePossibility = getRandomNumber(0, 13); 
-            while(shelf.get(demagePossibility).inventory == 0){ //check if the index game selected are still on inventory, if not, remove the next game on list
-                if(demagePossibility == 12){ 
-                    demagePossibility = 0;
-                }
-                else{
-                    demagePossibility++;
+            brokenGame = getRandomNumber(0, 13); 
+            if(shelf.get(brokenGame).inventory == 0){
+                while(shelf.get(brokenGame).inventory != 0){ //check if the index game selected are still on inventory, if not, remove the next game on list
+                    if(brokenGame == 12){ 
+                        brokenGame = 0;
+                    }
+                    else{
+                        brokenGame++;
+                    }
                 }
             }
-            shelf.get(demagePossibility).inventory--;
-            int count = DamageContainer.get(shelf.get(demagePossibility).game_name);
-            DamageContainer.put(shelf.get(demagePossibility).game_name, count++);
-            System.out.println("The game " +  shelf.get(demagePossibility).game_name + " is damaged and placed on the Damaged Game Container");
+            
+            shelf.get(brokenGame).inventory--; //subtract broken game from inventory
+            int count = DamageContainer.get(shelf.get(brokenGame).game_name);
+            DamageContainer.put(shelf.get(brokenGame).game_name, count++);
+            System.out.println("The game " +  shelf.get(brokenGame).game_name + " is damaged and placed on the Damaged Game Container");
         }
         else if(employee_name == "Ernie" & demagePossibility >= 0 & demagePossibility <= 5){
             //generate a random number from 1 to 12 (because there are 12 fixed game titles). The selected number will represent which game to remove
-            demagePossibility = getRandomNumber(0, 13);
-            while(shelf.get(demagePossibility).inventory == 0){ //check if the index game selected are still on inventory, if not, remove the next game on list
-                if(demagePossibility == 12){
-                    demagePossibility = 0;
-                }
-                else{
-                    demagePossibility++;
+            brokenGame = getRandomNumber(0, 13);
+            if(shelf.get(brokenGame).inventory == 0){
+                while(shelf.get(brokenGame).inventory != 0){ //check if the index game selected are still on inventory, if not, remove the next game on list
+                    if(brokenGame == 12){ 
+                        brokenGame = 0;
+                    }
+                    else{
+                        brokenGame++;
+                    }
                 }
             }
-            shelf.get(demagePossibility).inventory--;
-            int count = DamageContainer.get(shelf.get(demagePossibility).game_name);
-            DamageContainer.put(shelf.get(demagePossibility).game_name, count++);
-            System.out.println("The game " +  shelf.get(demagePossibility).game_name + " is damaged and placed on the Damaged Game Container");
+            shelf.get(brokenGame).inventory--; //subtract broken game from inventory
+            int count = DamageContainer.get(shelf.get(brokenGame).game_name); //NAO DEVERIA SER .GAME_INVENTORY???
+            DamageContainer.put(shelf.get(brokenGame).game_name, count++);
+            System.out.println("The game " +  shelf.get(brokenGame).game_name + " is damaged and placed on the Damaged Game Container");
         }
 
     }
@@ -93,19 +95,19 @@ public class Cashier extends Employee{
 
     public void Open(List<Games> shelf, Register reg){
         System.out.println(employee_name + " opened the store. Welcome!");
-        int num_costomers = getRandomNumber(0, 5);
-        System.out.println(num_costomers + " customer are in store today!");
-        int prob_buying = 0;
-        int decreaser = 0;
-        int num_game_bought = 0;
-        int customer_num = 1;
-        boolean bought_game = false;
-        for (int i = 0; i <= num_costomers; i++){
+        int num_customers = getRandomNumber(0, 5); //this variable will hold the number of customers 
+        System.out.println(num_customers + " customer are in store today!");
+        int prob_buying = 0; //this variable holds the probability of a customer buying a game
+        int decreaser = 0; //this is a helper variable, it exists to decrease the probility of a game being bought everytime a customer moves to another shelf.
+        int num_game_bought = 0; //this variable will hold how many games a customer bought per day. It exists to avoid a customer buying more than 2 games
+        int customer_num = 1; //this variable controls which customer is currently buying
+        boolean bought_game = false; //variable to check if a customer have bought any game
+        for (int i = 0; i <= num_customers; i++){ //loop throught all customer and perform their operations each one per time
             num_game_bought = 0;
-            while(num_game_bought < 3 && customer_num <= num_costomers){
+            while(num_game_bought < 3 && customer_num <= num_customers){ //while loop to gurantee that customers will not buy more than 2 games and also to make sure that only 4 customers max will perform actions on the store
                 decreaser = 0;
                 bought_game = false;
-                for(int k = 0; k < shelf.size(); k++){
+                for(int k = 0; k < shelf.size(); k++){ //loop throught all shelfs a single customer will take a look
                     prob_buying = getRandomNumber(0, 101);
                     if(shelf.get(i).inventory == 0){
                         // System.out.println("Empty shelf");
